f5772d8a68a213bd99509f9ef14cbd89fd96cc58
Kazuhide Oki <cedar123pc@gmail.com>
2020-03-15 13:54:28 +0800

    DisplayNumbersとLatestまで切り分け完了

--- src/PPagination.tsx
+++ src/PPagination.tsx
@@ -15,6 +15,9 @@ import { ThemeType } from './modules/ThemeContext';
 import { useStylesFactory } from './modules/useStylesFactory';
 import { Grid } from '@material-ui/core';
 
+import { DisplayNumbers } from './PPaginationModules/DisplayNumbers';
+import { Latest } from './PPaginationModules/Latest';
+
 const styles = {
     icon: {
         fontSize: (themes: ThemeType) => themes.iconSmall
@@ -36,6 +39,12 @@ const styles = {
         justifyContent: "center",
         width: 400
     }
+};
+
+export type pageArrowProps = {
+  setParams: (type: any) => void;
+  classesDisable?: string;
+  classesIcon?: string
 };
 
 type Props = {
@@ -96,25 +105,6 @@ const PPaginationPresenter = ({
         return <p className={classes.nums}>【 {currentPage}/{totalPages} 】</p>
     }
 
-    //  ページ数が3より大きい場合latestとoldestを表示
-    const Latest = () => {
-        const arg = { type: "LATEST" };
-        let onClick
-        let disable
-        if (currentPage > 3 && totalPages > 3) {
-            onClick = () => setParams(arg)
-            disable = null
-        } else{
-            onClick = undefined
-            disable = classes.disable;
-        } 
-        return (
-            <FirstPage
-            onClick={onClick}
-            className={`${classes.icon} ${disable}`}
-            />
-        );
-    };
 
     const Prev = () => {
         const arg = { type: "PREV" };
@@ -170,42 +160,6 @@ const PPaginationPresenter = ({
         );
     };
 
-    const number1 = currentPage - 2;
-    const number2 = currentPage - 1;
-    const number3 = currentPage;
-    const number4 = currentPage + 1;
-    const number5 = currentPage + 2;
-
-    const numbers = [number1, number2, number3, number4, number5];
-
-    const DisplayNumbers = () => {
-        const nums = numbers.map(num => {
-            if (num <= 0) {
-                return "";
-            } else if (num > totalPages) {
-                return "";
-            } else if (num === currentPage) {
-                return (
-                  <button
-                    key={num}
-                    className={`${classes.nums} ${classes.numsCurrent}`}
-                  >
-                    {num}
-                  </button>
-                );
-            }
-
-            const arg = { type: "NUM", payload: num };
-            return (
-                <button key={num} onClick={() => setParams(arg)} className={classes.nums}> 
-                    {num}
-                </button>
-            );
-        })
-
-        return <>{nums}</>
-    }
-
     const SelectParams = () => (
         <>
             <Grid item>
@@ -224,14 +178,14 @@ const PPaginationPresenter = ({
     )
 
     const Pagination = () => (
-        <Grid item className={classes.pagination}>
-            <Latest />
-            <Prev />
-            <DisplayNumbers />
-            <Next />
-            <Oldest />
-        </Grid>
-    )
+      <Grid item className={classes.pagination}>
+        <Latest setParams={setParams} classesDisable={classes.disable} classesIcon={classes.icon}/>
+        <Prev />
+        <DisplayNumbers setParams={setParams} />
+        <Next />
+        <Oldest />
+      </Grid>
+    );
 
     return (
         <Grid container justify="center" spacing={1}>
---
+++ src/PPaginationModules/Latest.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { FirstPage } from "@material-ui/icons";
+import { pageArrowProps } from "../PPagination";
+import { Store } from "../modules/Store";
+
+//  ページ数が3より大きい場合latestとoldestを表示
+export const Latest = (props: pageArrowProps) => {
+    const { wpParams, totalPages } = React.useContext(Store);
+    const currentPage = wpParams.currentPage;
+
+    const arg = { type: "LATEST" };
+    let onClick;
+    let disable;
+    if (currentPage > 3 && totalPages > 3) {
+        onClick = () => props.setParams(arg);
+        disable = null;
+    } else {
+        onClick = undefined;
+        disable = props.classesDisable;
+    }
+    return (
+        <FirstPage
+            onClick={onClick}
+            className={`${props.classesIcon} ${disable}`}
+        />
+    );
+};
