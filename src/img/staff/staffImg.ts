ded3daaf5e16ddea0fa54ca4ad2c0b24c791b778
kazuhideoki <cedar123pc@gmail.com>
2022-07-06 07:51:38 +0900

    Tomoさんを staffImgに追加

--- src/img/staff/staffImg.ts
+++ src/img/staff/staffImg.ts
@@ -1,6 +1,7 @@
 import img10 from "./img10.jpg";
 import img12 from "./img12.jpg";
 import img13 from "./img13.jpg";
+import img14 from "./img14.jpg";
 import img3 from "./img3.jpg";
 import img4 from "./img4.jpg";
 import img5 from "./img5.jpg";
@@ -9,7 +10,16 @@ import img7 from "./img7.jpg";
 import img8 from "./img8.jpg";
 
 export const staffImg = {
-    img10, img12, img13, img3, img4, img5, img6, img7, img8
-}
+  img10,
+  img12,
+  img13,
+  img14,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+};
 
-export type TstaffImg  = typeof staffImg;
+export type TstaffImg = typeof staffImg;
